9e957ee8258e3410198f5055627c8c8039cdfddb
Vivek <vivek.oss@linetra.com>
2024-09-23 17:23:21 +0530

    Add mount
    Old code from 2016. Committing without testing.

--- internet_status.sh
+++ internet_status.sh
@@ -3,6 +3,7 @@
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
         echo "Online"
+	./router_cmd.exp mount
 else
         echo "Offline"
 	./router_cmd.exp reboot
